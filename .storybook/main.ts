4f053b864b89ba8fa7a65d1d72afbcc577255492
ruokic <gusqo327@naver.com>
2024-05-29 19:53:57 +0900

    Feat: set default components
    - delete example files of storybook
    - move example button component to /app/ui/common
    - setup storybook setting for /app/ui
    - apply tailwindcss to Button.tsx
    - rename components to Pascal Case

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,8 +11,8 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/ui/**/*.mdx",
+    "../app/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
