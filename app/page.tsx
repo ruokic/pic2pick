c578776e4f2a8ec4c9df92bda88e9239c14f59ae
ruokic <gusqo327@naver.com>
2024-05-30 17:50:54 +0900

    Feat: divide sections in main
    - modify Uploader.tsx to do its own role
    - set sections in main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import PreviewImage from "@/app/ui/picture/PreviewImage";
+import { Picture } from "@/app/types/picture";
 import Uploader from "@/app/ui/picture/Uploader";
 
 export default function Home() {
+  const [pictures, setPictures] = useState<Picture[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const addPictures = useCallback((files: FileList) => {
+    setPictures((prev) =>
+      prev.concat(
+        Array.from(files).map((file) =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        )
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      pictures.map((picture) => URL.revokeObjectURL(picture.preview));
+    };
+  }, [pictures]);
+
   return (
-    <main>
-      <Uploader />
+    <main className="flex">
+      {/* Picture List */}
+      <div>
+        {pictures.map((picture) => (
+          <div key={picture.preview}>{picture.name}</div>
+        ))}
+        <Uploader pictures={pictures} addPictures={addPictures} />
+      </div>
+      {/* MetaData of Selected Picture */}
+      <div></div>
+      {/* Preview of Selected Picture */}
+      <div></div>
     </main>
   );
 }
--- app/ui/picture/Uploader.tsx
+++ app/ui/picture/Uploader.tsx
@@ -1,33 +1,19 @@
-"use client";
-import { useState, useEffect, useCallback } from "react";
-import PreviewImage from "@/app/ui/picture/PreviewImage";
 import { Picture } from "@/app/types/picture";
 
-export default function Uploader() {
-  const [picture, setPicture] = useState<Picture | null>();
+interface UploaderProps {
+  pictures: Picture[];
+  addPictures: (pictures: FileList) => void;
+}
 
-  const handleChangePicture = useCallback((file: File) => {
-    setPicture({ ...file, preview: URL.createObjectURL(file) });
-  }, []);
-
-  useEffect(() => {
-    return () => {
-      picture?.preview && URL.revokeObjectURL(picture.preview);
-    };
-  }, [picture]);
-
+export default function Uploader({ pictures, addPictures }: UploaderProps) {
   return (
-    <>
-      {picture && <PreviewImage picture={picture} />}
-      <input
-        id="file"
-        type="file"
-        accept="image/*"
-        className=""
-        onChange={(e) =>
-          e.target?.files && handleChangePicture(e.target.files[0])
-        }
-      ></input>
-    </>
+    <input
+      id="file"
+      type="file"
+      accept="image/*"
+      multiple
+      className=""
+      onChange={(e) => e.target?.files && addPictures(e.target.files)}
+    ></input>
   );
 }
