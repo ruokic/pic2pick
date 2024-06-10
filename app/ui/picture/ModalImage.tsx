280edfc44bb47c3a0a70860950e79c821230a481
ruokic <gusqo327@naver.com>
2024-06-10 19:30:07 +0900

    Feat: add image modal in picture/previewImage
    - add ModalImage in picture

---
+++ app/ui/picture/ModalImage.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Modal } from "@/app/ui/components";
+import { Picture } from "@/app/lib/types/picture";
+
+interface ModalImageProps {
+  picture: Picture;
+  showModal: boolean;
+  setShowModal: (nextShowModal: boolean) => void;
+}
+
+export default function ModalImage({
+  picture,
+  showModal,
+  setShowModal,
+}: ModalImageProps) {
+  return (
+    <Modal
+      showModal={showModal}
+      setShowModal={setShowModal}
+      style="max-w-[90vw] max-h-[90vh] justify-center relative"
+    >
+      <Image
+        src={picture.preview}
+        className="max-h-[calc(90vh-4rem)] h-full w-auto object-contain"
+        width={1920}
+        height={1080}
+        alt={picture.name}
+      />
+    </Modal>
+  );
+}
--- app/ui/picture/PreviewImage.tsx
+++ app/ui/picture/PreviewImage.tsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import Image from "next/image";
-import { Container } from "@/app/ui/components";
+import { Container, Modal } from "@/app/ui/components";
+import ModalImage from "@/app/ui/picture/ModalImage";
 import { usePictureStore } from "@/app/lib/store/usePictureStore";
 
 export default function PreviewImage() {
   const { pictures, selectedIndex } = usePictureStore();
+  const [showModal, setShowModal] = useState(false);
   const picture = pictures[selectedIndex];
 
   return (
-    <Container style="relative w-full min-h-80 h-2/3">
+    <Container style="relative w-full min-h-80 h-2/3 bg-gray-200">
       {picture ? (
-        <Image
-          src={picture.preview}
-          className="object-contain p-4"
-          fill
-          quality={100}
-          alt="uploaded picture"
-        />
+        <>
+          <button onClick={() => setShowModal(true)}>
+            <Image
+              src={picture.preview}
+              className="object-contain p-2"
+              fill
+              quality={100}
+              alt={picture.name}
+            />
+          </button>
+          <ModalImage
+            picture={picture}
+            showModal={showModal}
+            setShowModal={setShowModal}
+          />
+        </>
       ) : (
         <></>
       )}
