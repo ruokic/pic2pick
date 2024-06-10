b0e038adb7c7a7eb4531c95950e2e104cdd8f47a
ruokic <gusqo327@naver.com>
2024-06-10 23:24:33 +0900

    Feat: add message for no pictures in list
    - add message for no picture in picture/List

--- app/ui/picture/List.tsx
+++ app/ui/picture/List.tsx
@@ -33,40 +33,50 @@ export default function List() {
         </Text>
         <Uploader />
       </div>
-      <div className="flex px-2 gap-2">
-        <input
-          id="checkall"
-          type="checkbox"
-          checked={pictures?.length === checkedIndexSet?.size}
-          onChange={() =>
-            checkedIndexSet?.size < pictures?.length
-              ? checkAllIndex()
-              : uncheckAllIndex()
-          }
-        />
-        <label htmlFor="checkall">
-          <Text>전체 선택</Text>
-        </label>
-      </div>
-      <PictureList>
-        {pictures.map((picture, index) => (
-          <ListItem key={picture.preview} picture={picture} index={index} />
-        ))}
-      </PictureList>
-      <div className="flex flex-col gap-2 justify-self-end">
-        <Button
-          label="선택 다운로드"
-          primary
-          onClick={handleClickDownload}
-          disabled={checkedIndexSet.size === 0}
-        />
-        <Button
-          label="선택 삭제"
-          warning
-          onClick={deleteCheckedPictures}
-          disabled={checkedIndexSet.size === 0}
-        />
-      </div>
+      {pictures.length > 0 ? (
+        <>
+          <div className="flex px-2 gap-2">
+            <input
+              id="checkall"
+              type="checkbox"
+              checked={pictures?.length === checkedIndexSet?.size}
+              onChange={() =>
+                checkedIndexSet?.size < pictures?.length
+                  ? checkAllIndex()
+                  : uncheckAllIndex()
+              }
+            />
+            <label htmlFor="checkall">
+              <Text>전체 선택</Text>
+            </label>
+          </div>
+
+          <PictureList>
+            {pictures.map((picture, index) => (
+              <ListItem key={picture.preview} picture={picture} index={index} />
+            ))}
+          </PictureList>
+          <div className="flex flex-col gap-2 justify-self-end">
+            <Button
+              label="선택 다운로드"
+              primary
+              onClick={handleClickDownload}
+              disabled={checkedIndexSet.size === 0}
+            />
+            <Button
+              label="선택 삭제"
+              warning
+              onClick={deleteCheckedPictures}
+              disabled={checkedIndexSet.size === 0}
+            />
+          </div>
+        </>
+      ) : (
+        <div className="h-full flex flex-col justify-center items-center">
+          <Text>사진이 없습니다</Text>
+          <Text>새로 추가해 보세요</Text>
+        </div>
+      )}
     </Container>
   );
 }
