9a70aace713ebc1cc72f24f7ad2144da7d77a807
ruokic <gusqo327@naver.com>
2024-06-13 20:59:33 +0900

    Feat: add picture page
    - add picture page
    - remove picture in home
    - set redirect to picture at home

---
+++ app/picture/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import { Divider } from "@/app/ui/components";
+import PictureList from "@/app/ui/picture/List";
+import PictureDetail from "@/app/ui/picture/Detail";
+import { useHeightResizer, useWidthResizer } from "@/app/lib/hooks";
+import { usePictureStore } from "@/app/lib/store";
+
+export default function Picture() {
+  const { containerRef, handleMouseDown } = useWidthResizer();
+  const { pictures } = usePictureStore();
+
+  useEffect(() => {
+    return () => {
+      pictures.forEach((picture) => picture.revokePreview());
+    };
+  }, []);
+
+  return (
+    <main ref={containerRef} className="flex h-[calc(100vh-66px)]">
+      <PictureList />
+      <Divider resizable onMouseDown={handleMouseDown} />
+      <PictureDetail />
+    </main>
+  );
+}
